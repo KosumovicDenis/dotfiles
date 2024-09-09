fac0b8dea56bea1504bddcc2365a03cbd7811786
KosumovicDenis <deniskosumi21@gmail.com>
2024-09-09 21:24:55 +0200

    Fixed PROMPT error. Add go path

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,21 +1,24 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+-- local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- Settings
 -- Color & Window conf
-config.color_scheme = 'Tokyo Night'
+config.color_scheme = 'MaterialDesignColors'
 config.window_background_opacity = 0.9
-config.window_decorations = "RESIZE"
+config.window_decorations = 'RESIZE'
 config.window_close_confirmation = 'AlwaysPrompt'
 config.scrollback_lines = 3000
-config.default_workspace = "home"
+config.default_workspace = 'home'
 -- Font
 config.font = wezterm.font('Hack Nerd Font Mono')
 config.font_size = 15
 config.line_height = 1.05
+-- Keys
+-- config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
 
 return config
 
